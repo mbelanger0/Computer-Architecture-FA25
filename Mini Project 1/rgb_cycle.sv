f9a5c43124325d6746193745b7ce15e49bc7f944
Mark Belanger <mbelanger@olin.edu>
2025-09-10 21:30:26 -0400

    Add Mini Project 1 implementation and blink example

--- Mini Project 1/rgb_cycle.sv
+++ Mini Project 1/rgb_cycle.sv
@@ -35,35 +35,64 @@ module top(
 );
 
     parameter FADE_INTERVAL = 2000000;
-    logic [$clog2(FADE_INTERVAL) - 1:0] count = 0;
+    logic [$clog2(6 * FADE_INTERVAL) - 1:0] count = 0;
 
     initial begin
-        RGB_R = 1'b1;
+
+        RGB_R = 1'b0;
         RGB_G = 1'b1;
         RGB_B = 1'b1;
 
-        parameter ON = 1'b0;
-        parameter OFF = 1'b1;
     end
 
     always_ff @(posedge clk) begin
 
-        // Red
-        if (count == 0 * FADE_INTERVAL) begin
-            RGB_R = 1'b0;
-        end
+        // // Red
+        // if (count == 0 * FADE_INTERVAL) begin
+        //     RGB_R <= ~RGB_R;
+        //     count <= count + 1;
+        // end
+
 
         // Yellow
-        else if(count == FADE_INTERVAL) begin
-            RGB_G = 1'b0;
+        if(count == 1 * FADE_INTERVAL-1) begin
+            RGB_G <= ~RGB_G;
+            count <= count + 1;
         end
 
         // Green
-        else if(count == 2 * FADE_INTERVAL) begin
-            RGB_R = 1'b1;
+        else if(count == 2 * FADE_INTERVAL-1) begin
+            RGB_R <= ~RGB_R;
+            count <= count + 1;
         end
 
+        // Cyan
+        else if(count == 3 * FADE_INTERVAL-1) begin
+            RGB_B <= ~RGB_B;
+            count <= count + 1;
+        end
 
+        // Blue
+        else if(count == 4 * FADE_INTERVAL-1) begin
+            RGB_G <= ~RGB_G;
+            count <= count + 1;
+        end
 
+        // Magenta
+        else if(count == 5 * FADE_INTERVAL-1) begin
+            RGB_R <= ~RGB_R;
+            count <= count + 1;
+        end
+
+        else if(count == 6 * FADE_INTERVAL-1) begin
+            RGB_B <= ~RGB_B;
+            count <= 0;
+        end
+        
+        else begin
+            count <= count + 1;
+        end
     end
+    
+endmodule
 
